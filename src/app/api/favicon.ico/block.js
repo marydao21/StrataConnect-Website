47d8b584836cb5c46f3df34bf819c0a2159fc79b
marydao21 <daongocminh210104@gmail.com>
2025-04-04 11:15:42 +1100

    update all

--- src/app/api/favicon.ico/block.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  // Stop logging favicon by returning a no-content response
-  return new NextResponse(null, { status: 204 });
-}
